58fbf3e5b3bc28bf8d3ca6b7af8b964e75f2f007
Fabio Fattori <fabiofattoriw@gmail.com>
2022-11-13 21:28:59 +0100

    Gestione Messaggi troppo Lunghi

--- src/Componenti/ListaMex.jsx
+++ src/Componenti/ListaMex.jsx
@@ -2,9 +2,54 @@ import React, { useState, useEffect } from "react";
 import { getMex } from "../Functions/Store";
 import "./Mex.css";
 
-function ListaMex({UserName}) {
+function ListaMex({ UserName }) {
   var Inviati;
   const [GraficaMexs, setGrafica] = useState();
+
+  function StampaMessaggiLunghi(ID, Messaggio, index) {
+    var toReturn;
+    var string="";
+    if (ID === "MexRicevuto") {
+      for (let i = 0; i < Messaggio.length; i++) {
+        for (let j = 0; j < 40; j++) {
+          string += Messaggio[j];
+          i = j;
+        }
+        string += " \n";
+      }
+
+      return (toReturn = (
+        <div key={index} className="MexRicevuto ClasseInvio">
+          <p className="rimpiazzo">o</p>
+          <p className="ValueTroppoGrossa">
+            <span>{string}</span>
+          </p>
+        </div>
+      ));
+    } else if (ID === "MexInviato") {
+      console.log(Messaggio.length)
+      for (let i = 0; i < Messaggio.length; i++) {
+        for (let j = 0; j < 40; j++,i++) {
+          string += Messaggio[j];
+          console.log(string)
+          
+          
+          console.log(i)
+        }
+        console.log(string)
+        string += " \n";
+      }
+
+      return (toReturn = (
+        <div key={index} className="MexInviato ClasseInvio">
+          <p className="rimpiazzo">o</p>
+          <p className="ValueTroppoGrossa">
+            <span>{string}</span>
+          </p>
+        </div>
+      ));
+    }
+  }
 
   function CreaGrafica() {
     var m = [];
@@ -13,17 +58,37 @@ function ListaMex({UserName}) {
         Inviati[index].Sender === "me" &&
         Inviati[index].To === localStorage.getItem("Select")
       ) {
-        m[index] = (
-          <div key={index} className="MexRicevuto">
-            <p className="Value"><span>{Inviati[index].value}</span></p>
-          </div>
-        );
+        if (Inviati[index].value.length > 24) {
+          m[index] = StampaMessaggiLunghi(
+            "MexRicevuto",
+            Inviati[index].value,
+            index
+          );
+        } else {
+          m[index] = (
+            <div key={index} className="MexRicevuto">
+              <p className="Value">
+                <span>{Inviati[index].value}</span>
+              </p>
+            </div>
+          );
+        }
       } else if (Inviati[index].Sender === localStorage.getItem("Select")) {
-        m[index] = (
-          <div key={index} className="MexInviato">
-            <p className="Value"><span>{Inviati[index].value}</span></p>
-          </div>
-        );
+        if (Inviati[index].value.length > 24) {
+          m[index] = StampaMessaggiLunghi(
+            "MexInviato",
+            Inviati[index].value,
+            index
+          );
+        } else {
+          m[index] = (
+            <div key={index} className="MexInviato">
+              <p className="Value">
+                <span>{Inviati[index].value}</span>
+              </p>
+            </div>
+          );
+        }
       }
     }
 
@@ -34,7 +99,7 @@ function ListaMex({UserName}) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     Inviati = getMex();
     CreaGrafica();
-  },[UserName]);
+  }, [UserName]);
 
   return <div className="MexContainer">{GraficaMexs}</div>;
 }
